dfa00982a634039dd6ddb62264c709f08a52c2c7
Maxine <hmx222@gmail.com>
2024-02-27 08:34:05 -0700

    Set up databases and models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Booking(models.Model):
+    TableID=models.IntegerField()
+    Name=models.CharField(max_length=255)
+    No_of_guests=models.IntegerField()
+    BookingDate=models.DateField(default=datetime.date.today)
+
+class Menu(models.Model):
+    MenuID=models.IntegerField()
+    Title=models.CharField(max_length=255)
+    Price=models.DecimalField(max_digits=10,decimal_places=2)
+    Inventory=models.IntegerField()
